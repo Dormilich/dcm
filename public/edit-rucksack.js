061a181e095df909ac4583b46eeb7d8af6370537
Bertold von Dormilich <Dormilich@netscape.net>
2014-03-26 11:09:03 +0100

    Changing equipment editor to using Bootstrap.

--- public/edit-rucksack.js
+++ public/edit-rucksack.js
@@ -1,20 +1,19 @@
 $('form').on("submit", function() {
-	$(this).find("tfoot").remove();
+	$(this).find(".panel-footer").remove();
 	$(this).find("input[type='number']").each(function() {
 		if (0 == this.value) {
-			$(this).closest("tr").remove();
+			$(this).closest(".form-group").remove();
 		}
 	});
 });
 $('#add').on("click", function() {
-	var idx  = $(this).closest('table').find('tbody tr').length;
-	var $row = $(this).closest('tr');
+	var idx  = $(this).closest('.panel').find('.panel-body .form-group').length;
+	var $row = $(this).closest('.panel-footer').find('.form-group');
 	var $tr  = $row.clone();
-	$tr.find('button').remove();
 	$tr.find('*[name]').each(function() {
 		this.name = this.name.replace("{index}", idx);
 	});
-	$(this).closest('table').append($tr);
+	$(this).closest('.panel').find('.panel-body .container-fluid').append($tr);
 	$row.find('input').each(function() {
 		this.value = this.defaultValue;
 	});
