3b7054640966d326d5cebd67b2c7b56f7ea1231c
Bertold von Dormilich <Dormilich@netscape.net>
2014-07-05 08:38:28 +0200

    getting ordained char sheet to work with bootstrap.

--- public/edit-liturgie.js
+++ public/edit-liturgie.js
@@ -1,36 +1,29 @@
-$('table').on('click', 'label', function() {
-	// click is faster than label => checkbox
-	var $tr    = $(this).closest('tr');
-	var $check = $tr.find('.isactive');
-	if ($check.length) {
-		$tr.find('.unlock').prop('disabled', $check.prop('checked'));
-		$tr.toggleClass('selected');
-	}
-}).on('click', 'input[type="checkbox"]', function() {
-	var $tr    = $(this).closest('tr');
+$('button').prop('disabled', false);
+
+$('.panel').on('click', 'input[type="checkbox"]', function() {
+	var $tr    = $(this).closest('.row');
 	var $check = $tr.find('.isactive');
 	if ($check.length) {
 		$tr.find('.unlock').prop('disabled', !$check.prop('checked'));
 		$tr.toggleClass('selected');
 	}
 });
-$('form').on('submit', function() {
-	$('#template').remove();
-});
+
 $('#addlk').on('click', function() {
 	var $option = $('#newlk :selected');
-	var $row    = $('#template tr').clone();
-	var index   = $(this).closest('table').find('#collection tr').length;
-	var $sec = $row.find('input').eq(1);
-	$sec.val($option.val());
-	$sec.prop('id', $sec.prop('id').replace("{index}", index));
-	var $lab = $row.find('label');
-	$lab.text($option.val());
+	var $row    = $('#template .row').clone();
+	var index   = $('#collection').find('.row').length;
+	var $name	= $row.find('input.isactive');
+	$name.val($option.text());
+	$name.prop('id', $name.prop('id').replace("{index}", index));
+	var $lab 	= $row.find('label');
+	$lab.text($option.text());
 	$lab.prop('for', $lab.prop('for').replace("{index}", index));
-	$row.find('input').eq(0).val($option.data('id'));
+	$row.find('input').eq(0).val($option.val());
 	$row.find('select').val('E');
 	$row.find('*[name]').each(function() {
 		this.name = this.name.replace("{index}", index);
 	});
 	$('#collection').append($row);
+	$option.remove();
 });
