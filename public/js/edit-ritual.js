199e52c1702ea1fd56b2ce1d8c2e6864fd139d78
Bertold von Dormilich <Dormilich@netscape.net>
2014-07-07 19:34:21 +0200

    finishing rituals.

--- public/js/edit-ritual.js
+++ public/js/edit-ritual.js
@@ -38,12 +38,53 @@ function copyStd(data)
 
 function copyZtz(data)
 {
-	var $row = $('#templates .ztz').clone();
+	var $row   = $('#templates .ztz').clone();
+	var $check = $row.find('input.isactive');
+	var $label = $row.find('label');
+	
+	$row.find('input.long').val(data.repLong);
+	$row.find('input.short').val(data.repShort);
+
+	$row.find('*[name]').each(function() {
+		this.name = this.name.replace("{index}", data.index);
+	});
+	$check.prop('id', $check.prop('id').replace("{index}", data.index));
+
+	$label.text($label.text() + ' (' + data.repLong + ')');
+	$label.prop('for', $label.prop('for').replace("{index}", data.index));
+
+	$row.find('select').val('E');
+
+	return $row;
 }
 
 function copyShm(data)
 {
-	var $row = $('#templates .shm').clone();
+	var $row   = $('#templates .shm').clone();
+	var $check = $row.find('input.isactive');
+	var $label = $row.find('label');
+	var index;
+	
+	$row.find('input.long').val(data.repLong);
+	$row.find('input.short').val(data.repShort);
+
+	index  = data.index;
+	$row.find('*[name]').each(function() {
+		this.name = this.name.replace("{index}", index++);
+	});
+	index = data.index;
+	$check.each(function () {
+		this.id = this.id.replace("{index}", index++);
+	});
+	index = data.index;
+	$label.each(function () {
+		this.textContent += ' (' + data.repLong + ')';
+		this.htmlFor      = this.htmlFor.replace("{index}", index++)
+	});
+	
+	$row.find('select').val('E');
+
+	return $row;
 }
 
 $('#addrk').on('click', function () {
