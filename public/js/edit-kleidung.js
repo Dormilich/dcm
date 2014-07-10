bf3b707cf04f5a4bdd3c0e01a397741e4029da99
Bertold von Dormilich <Dormilich@netscape.net>
2014-07-10 22:01:59 +0200

    making armour work.

--- public/js/edit-kleidung.js
+++ public/js/edit-kleidung.js
@@ -1,15 +1,7 @@
-Number.prototype.round = function (digits) {
-	return parseFloat(this.toFixed(digits));
-}
-
 $('button').prop("disabled", false);
 
-$('form').on("submit", function() {
-	$(this).find(".panel-footer").remove();
-});
-
 $('#armor').on("click", '.isactive', function() {
-	$(this).closest('.row').find('*[name]').prop("disabled", !this.checked);
+	$(this).closest('li').find('*[name]').prop("disabled", !this.checked);
 });
 
 $('#liste').on("click", 'input', function() {
@@ -33,20 +25,20 @@ $('#liste').on("click", 'input', function() {
 });
 
 $('#add').on("click", function() {
-	var idx  = $(this).closest('.panel').find('.panel-body .row').length;
-	var $row = $(this).closest('.row');
-	var $tr  = $row.clone();
-	$tr.find('button').remove();
-	$tr.find('*[name]').each(function () {
+	var idx  = $(this).closest('.panel').find('li').length;
+	var $src = $(this).closest('.panel-footer').find('input[type="number"]');
+	var $row = $('#template').find('li').clone();
+	var $inp = $row.find('input[type="number"]');
+	
+	$row.find('*[name]').each(function () {
 		this.name = this.name.replace("{index}", idx);
 	});
-	$(this).closest('.panel').find('.panel-body').append($tr);
-	$row.find('input').each(function () {
-		this.value = this.defaultValue;
+	$src.each(function (index) {
+		$inp[index].value = this.value;
 	});
-	$tr.find('input').each(function () {
-		this.id = '';
-	});
+	$('#armor').append($row);
+
+	$src.val(0);
 }).on("click", function() {
 	$('#liste').find(':checked').prop("checked", false);
 });
